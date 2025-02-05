9e1ab441fddf78d3e3b751df2fec93ed01c46eea
Zorana Bozic <bozic.zorana@gmail.com>
2025-02-05 23:57:52 +0200

    Reinitialize repository

---
+++ fitness-challenge/src/app/_not-found.tsx/page.tsx
@@ -0,0 +1,10 @@
+export const dynamic = "force-dynamic";
+
+export default function NotFoundPage() {
+  return (
+    <div className="text-center">
+      <h1 className="text-2xl font-bold">Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
+}
