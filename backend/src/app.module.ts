ba0e2e474ed25a81097bd544f3392c96019a0a71
Zorana Bozic <bozic.zorana@gmail.com>
2025-02-06 00:22:21 +0200

    Reinitialize repository

---
+++ backend/src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: false, // ⚠️ Remove this in production!
+      }),
+    }),
+  ],
+})
+export class AppModule {}
