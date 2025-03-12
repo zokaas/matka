f3c4abbf3d77ff56fc7279aff986ebf4185bf9c0
Zorana Bozic <97917358+zokaas@users.noreply.github.com>
2025-03-12 22:25:29 +0200

    Update NavBar.tsx

--- fitness-challenge/src/app/components/NavBar.tsx
+++ fitness-challenge/src/app/components/NavBar.tsx
@@ -104,7 +104,7 @@ export default function Navbar() {
               Tilastot
             </Link>
             <Link
-              href="/ProgressDashboard"
+              href="/dashboard"
               className="hover:bg-purple-500 px-3 py-2 rounded text-sm font-medium"
             >
               Lisää tilastoja
@@ -202,7 +202,7 @@ export default function Navbar() {
             Tilastot
           </Link>
           <Link
-            href="/ProgressDashboard"
+            href="/dashboard"
             className="block px-4 py-2 text-sm font-medium hover:bg-purple-800"
             onClick={() => setIsMenuOpen(false)}
           >
