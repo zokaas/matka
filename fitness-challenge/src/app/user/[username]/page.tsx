f4b22584a33539c2732b1e6b7e82e6bca1fac1f0
Zorana Bozic <97917358+zokaas@users.noreply.github.com>
2025-08-21 20:48:36 +0300

    Update page.tsx

--- fitness-challenge/src/app/user/[username]/page.tsx
+++ fitness-challenge/src/app/user/[username]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-export const dynamic = “force-dynamic”;
+export const dynamic = "force-dynamic";
 
 import { useCallback, useEffect, useRef, useState, useMemo } from “react”;
 import { useParams } from “next/navigation”;
