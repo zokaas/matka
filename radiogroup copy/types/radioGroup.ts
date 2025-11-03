bfef672268a65b00c10dfa0bbbfdda2852a2d42e
Zorana Bozic <bozic.zorana@gmail.com>
2025-11-03 10:54:14 +0200

    t

---
+++ radiogroup copy/types/radioGroup.ts
@@ -0,0 +1,30 @@
+import { T_ErrorClassNamesProps } from "@ui/error";
+import { T_LabelInfo } from "@ui/label";
+
+export type T_Option = {
+    text: string;
+    value: number | string;
+};
+
+export type T_ClassNamesProps = {
+    radioRoot?: string;
+    radioItem?: string;
+    radioLabel?: string;
+    radioItemLabel?: string;
+    radioItemContainer?: string;
+    radioIndicator?: string;
+    errorContainer?: string;
+};
+
+export type T_RadioGroupProps = {
+    label: string;
+    fieldName: string;
+    onChange: (value: string) => void;
+    onBlur?: (fieldName: string) => void;
+    options: Array<T_Option>;
+    defaultValue?: string;
+    classNames?: T_ClassNamesProps;
+    error?: string;
+    errorClassNames?: T_ErrorClassNamesProps;
+    infoItems?: T_LabelInfo[] | null;
+};
